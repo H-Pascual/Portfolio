219edc86245be4f3149858774c6da8809cadb5d7
H-Pascual <hectorpascualmarin@gmail.com>
2024-12-14 02:44:04 +0100

    Añadiendo css y estilos

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import HeroComponent from './components/Hero'
-import ContactForm from './components/Contact'
-import WorkHistory from "./components/WorkHistory"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import AboutMe from "./components/AboutMe"
+import HeroComponent from './components/hero/Hero'
+import ContactForm from './components/contact/Contact'
+import WorkHistory from "./components/work-history/WorkHistory"
+import Skills from "./components/skills/Skills"
+import Projects from "./components/projects/Projects"
+import AboutMe from "./components/about-me/AboutMe"
 
 const Content = () => {
     return (<>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,16 @@
 import React from "react"
+import './Header'
 
 const Header = () => {
-    return (<header className="bg-black text-white">
-        <p><a href='#hero'>Héctor Pascual Marín</a></p>
-        <ul>
-            <li><a href='#hero'>INICIO</a></li>
-            <li><a href='#about-me'>SOBRE MI</a></li>
-            <li><a href='#knowledge'>CONOCIMIENTOS</a></li>
-            <li><a href='#projects'>PROYECTOS</a></li>
-            <li><a href='#work-history'>TRAYECTORIA</a></li>
-            <li><a href='#contact'>CONTACTO</a></li>
+    return (<header className="bg-gradient-to-r from-neutral-900 to-emerald-900 text-white py-1 px-2 flex flex-row static justify-between">
+        <p><a href='/#' className="">Héctor Pascual Marín</a></p>
+        <ul className='flex flex-row'>
+            <li className="mx-1 hover:underline"><a href='/#'>INICIO</a></li>
+            <li className="mx-1 hover:underline"><a href='#about-me'>SOBRE MI</a></li>
+            <li className="mx-1 hover:underline"><a href='#knowledge'>CONOCIMIENTOS</a></li>
+            <li className="mx-1 hover:underline"><a href='#projects'>PROYECTOS</a></li>
+            <li className="mx-1 hover:underline"><a href='#work-history'>TRAYECTORIA</a></li>
+            <li className="mx-1 hover:underline"><a href='#contact'>CONTACTO</a></li>
         </ul>
     </header>)
 }
