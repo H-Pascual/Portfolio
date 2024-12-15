4b80b0f31cc38f72797e3b6eae2533151d707951
H-Pascual <hectorpascualmarin@gmail.com>
2024-12-15 15:24:16 +0100

    Cambiando colores header y agregando footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react"
 
+//FIXME - Crear animación header
 const Footer = () => {
-    return (<footer></footer>)
+    return (<footer className="bg-gradient-to-r from-neutral-900 from-95% to-emerald-700 text-white py-1 px-2 flex flex-row static justify-between">
+        <p><a href='/#'>Héctor Pascual Marín 2024</a></p>
+        <ul className='flex flex-row'>
+            <li className="mx-1 hover:underline"><a href='/#'>INICIO</a></li>
+            <li className="mx-1 hover:underline"><a href='#about-me'>SOBRE MI</a></li>
+            <li className="mx-1 hover:underline"><a href='#knowledge'>CONOCIMIENTOS</a></li>
+            <li className="mx-1 hover:underline"><a href='#projects'>PROYECTOS</a></li>
+            <li className="mx-1 hover:underline"><a href='#work-history'>TRAYECTORIA</a></li>
+            <li className="mx-1 hover:underline"><a href='#contact'>CONTACTO</a></li>
+        </ul>
+    </footer>)
 }
 
 export default Footer
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import './Header'
 
+//FIXME - Agregar pestaña de notificaciones, cambio de idioma, cambio de colores...
 const Header = () => {
-    return (<header className="bg-gradient-to-r from-neutral-900 to-emerald-900 text-white py-1 px-2 flex flex-row static justify-between">
+    return (<header className="bg-gradient-to-r from-neutral-900 from-60% to-emerald-700 text-white py-1 px-2 flex flex-row static justify-between">
         <p><a href='/#' className="">Héctor Pascual Marín</a></p>
         <ul className='flex flex-row'>
             <li className="mx-1 hover:underline"><a href='/#'>INICIO</a></li>
